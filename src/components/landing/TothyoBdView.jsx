89cfdd2746aead660097e845010ea671a8d7c9a2
Azmain Amin <mdazmainamin@gmail.com>
2018-11-01 22:13:43 -0400

    added Header to landing page

---
+++ src/components/landing/TothyoBdView.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Header from "../header/Header";
+
+class TothyoBdView extends Component {
+    render(){
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+
+    }
+}
+
+export default TothyoBdView;
